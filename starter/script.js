7ed47d6eaff84b805f36ae60613b3376e7f3e464
MITHUN KUMAR SONI <mkumarsoni75@gmail.com>
2021-01-13 23:15:02 +0530

    completed

--- starter/script.js
+++ starter/script.js
@@ -13,52 +13,35 @@ console.log(document.querySelector('.guess').value);*/
 let secretNumber  = Math.trunc(Math.random() * 20);
 let score = 20;
 let highScore = Number(document.querySelector('.highscore').textContent);
-document.querySelector('.number').textContent = secretNumber;
+//document.querySelector('.number').textContent = secretNumber;
 
 
 document.querySelector('.again').addEventListener('click',function(){
     secretNumber = Math.trunc(Math.random() * 20);
     highScore = Number(document.querySelector('.highscore').textContent);
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('body').style.backgroundColor= 'black';
+    document.querySelector('.score').textContent = 20;
+    document.querySelector('.guess').value = '';
+    document.querySelector('.message').textContent = 'Start guessing...';
 
 })
 document.querySelector('.check').addEventListener
 ('click', function(){
+   
 const guess = Number(document.querySelector('.guess').value);
+
+
 // when there is no input
 if(!guess){
     document.querySelector('.message').textContent = 'No Number';
 }else if(guess === secretNumber){
     document.querySelector('body').style.backgroundColor = 'rgb(34, 218, 28)';
     document.querySelector('.number').style.width = '30rem'
+    document.querySelector('.number').textContent = secretNumber;
+
 
 
     document.querySelector('.message').textContent = 'Correct Number';
 
     if(score > highScore)
-    document.querySelector('.highscore').textContent = score;
-   // when guess is too high
-}else if(guess > secretNumber){
-    if(score > 0){
-        document.querySelector('.message').textContent = 'Too High!';
-        score--;
-        document.querySelector('.score').textContent = score;
-    }else{
-        document.querySelector('.message').textContent = 'You Lost the game!';
-    }
-    
-}else{
-    if(score > 0){
-        document.querySelector('.message').textContent = 'Too Low!';
-        score--;
-        document.querySelector('.score').textContent = score;
-    }else{
-        document.querySelector('.message').textContent = 'You Lost the game!';
-    }
-   
-}
-})
-
-
-
-
-
